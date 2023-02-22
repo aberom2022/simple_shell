c518a2899a7120e55220dbf04cf54b468fb000d3
Abel Adisu <abeladisu73@gmail.com>
2023-02-22 12:52:29 -0800

    updated

--- errors1.c
+++ errors1.c
@@ -1,10 +1,9 @@
 #include "shell.h"
-
 /**
  * _erratoi - converts a string to an integer
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
- *          -1 on error
+ *       -1 on error
  */
 int _erratoi(char *s)
 {
@@ -12,8 +11,8 @@ int _erratoi(char *s)
 	unsigned long int result = 0;
 
 	if (*s == '+')
-		s++; /* TODO; why does this make main return 255? */
-	for (i = 0; s[i] != '\0'; i++)
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
@@ -31,9 +30,9 @@ int _erratoi(char *s)
 /**
  * print_error - prints an error message
  * @info: the parameter & return info struct
- * @estr: string cointaining specified error type
+ * @estr: string containing specified error type
  * Return: 0 if no numbers in string, converted number otherwise
- *         -1 an error
+ *        -1 on error
  */
 void print_error(info_t *info, char *estr)
 {
@@ -84,7 +83,6 @@ int print_d(int input, int fd)
 
 	return (count);
 }
-
 /**
  * convert_number - converter function, a clone of itoa
  * @num: number
@@ -93,7 +91,7 @@ int print_d(int input, int fd)
  *
  * Return: string
  */
-char *convert_num(long int num, int base, int flags) 
+char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
 	static char buffer[50];
@@ -111,7 +109,7 @@ char *convert_num(long int num, int base, int flags)
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do      {
+	do	{
 		*--ptr = array[n % base];
 		n /= base;
 	} while (n != 0);
@@ -138,3 +136,5 @@ void remove_comments(char *buf)
 			break;
 		}
 }
+
+
