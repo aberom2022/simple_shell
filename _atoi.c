010a8cc7ec57f89f3de5c805aff6ff2d24ec89c2
Abel Adisu <abeladisu73@gmail.com>
2023-02-22 12:33:58 -0800

    modified

--- _atoi.c
+++ _atoi.c
@@ -1,12 +1,10 @@
 #include "shell.h"
-
 /**
- * interactive - return true if shell is interactive mode
+ * interactive - returns true if shell is interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
@@ -18,7 +16,6 @@ int interactive(info_t *info)
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -26,12 +23,10 @@ int is_delim(char c, char *delim)
 			return (1);
 	return (0);
 }
-
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to input
- *
- * Return: 1 if c is alphabetic, o otherwise
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
 
 int _isalpha(int c)
@@ -45,7 +40,6 @@ int _isalpha(int c)
 /**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
- *
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
@@ -70,9 +64,11 @@ int _atoi(char *s)
 	}
 
 	if (sign == -1)
-		output == -result;
+		output = -result;
 	else
 		output = result;
 
 	return (output);
+}
+
 }
